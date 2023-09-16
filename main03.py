9fdef5b577a9bf097efa967f4abdf6cd43bdf90e
01Evin <129361256+01Evin@users.noreply.github.com>
2023-09-16 23:40:07 +0800

    Update main03.py

--- main03.py
+++ main03.py
@@ -1,12 +1,6 @@
 sd0="s"+"t"+"a"+"b"+"l"+"e"+"-"+"d"+"i"+"f"+"f"+"u"+"s"+"i"+"o"+"n"+"-"+"w"+"e"+"b"+"u"
 sd=sd0+'i'
 %cd /content/{colabtools}
-#@markdown ####全精度/半精度(推荐)启动：##
-is_full_precision = False  # @param {type:'boolean'}
-#@markdown ####主题切换为暗配色：##
-is_black = False  # @param {type:'boolean'}
-#@markdown ####(可选)获取[ngrok](https://dashboard.ngrok.com/get-started/your-authtoken)的token进行免费网络加速：##
-ngrok_auth=""  #@param {type:"string"}
 
 full_precision_str="--share --lowram --disable-safe-unpickle  --disable-console-progressbars --xformers --enable-insecure-extension-access --precision full --no-half --no-half-vae --opt-sub-quad-attention --opt-channelslast --api"
 half_precision_str="--share --lowram  --disable-safe-unpickle  --disable-console-progressbars --xformers --enable-insecure-extension-access  --opt-sub-quad-attention --opt-channelslast --api"
