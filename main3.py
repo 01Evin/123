862080bf0d29d5b0490bf52304773134e93d17b8
01Evin <129361256+01Evin@users.noreply.github.com>
2023-09-18 00:26:33 +0800

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,16 +1,16 @@
-sd1="s"+"t"+"a"+"b"+"l"+"e"+"-"+"d"+"i"+"f"+"f"+"u"+"s"+"i"+"o"+"n"
-sd0="s"+"t"+"a"+"b"+"l"+"e"+"-"+"d"+"i"+"f"+"f"+"u"+"s"+"i"+"o"+"n"+"-"+"w"+"e"+"b"+"u"
-sd=sd0+'i'
-wi='w'+'e'+'b'+'u'+'i'
-ATM='A'+'U'+'T'+'O'+'M'+'A'+'T'+'I'+'C'+'1'+'1'+'1'+'1'
 import json
 import re
 import os
+import subprocess
+import sys
+wget_path = '/usr/bin/wget'
 
-  if params["image"] == "True":
-      json_content["outdir_txt2img_samples"] = "/content/drive/MyDrive/outputs/txt2img-images"
-      json_content["outdir_img2img_samples"] = "/content/drive/MyDrive/outputs/img2img-images"
-      json_content["outdir_extras_samples"] = "/content/drive/MyDrive/outputs/extras-images"
-      json_content["outdir_txt2img_grids"] = "/content/drive/MyDrive/outputs/txt2img-grids"
-      json_content["outdir_img2img_grids"] = "/content/drive/MyDrive/outputs/img2img-grids"
-  print("完成配置")
+params = {}
+    if params["image"] == "True":
+        json_content["outdir_txt2img_samples"] = "/content/drive/MyDrive/outputs/txt2img-images"
+        json_content["outdir_img2img_samples"] = "/content/drive/MyDrive/outputs/img2img-images"
+        json_content["outdir_extras_samples"] = "/content/drive/MyDrive/outputs/extras-images"
+        json_content["outdir_txt2img_grids"] = "/content/drive/MyDrive/outputs/txt2img-grids"
+        json_content["outdir_img2img_grids"] = "/content/drive/MyDrive/outputs/img2img-grids"
+    with open(f'{params["dir"]}/config.json', 'w') as configFile:
+        json.dump(json_content, configFile, ensure_ascii=False, indent=4)
