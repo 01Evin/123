23c4fdac70356c8b38acabf3c94e187d7f35dd35
01Evin <129361256+01Evin@users.noreply.github.com>
2023-09-18 22:49:50 +0800

    Update main4.py

--- main4.py
+++ main4.py
@@ -1,5 +1,3 @@
-sd0="s"+"t"+"a"+"b"+"l"+"e"+"-"+"d"+"i"+"f"+"f"+"u"+"s"+"i"+"o"+"n"+"-"+"w"+"e"+"b"+"u"
-sd=sd0+'i'
 import concurrent.futures
 import time
 import subprocess
@@ -8,7 +6,7 @@ import string
 import os
 
 # 指定要进入的文件夹路径
-folder_path = "/content/colabtools"
+folder_path = "/content/drive/MyDrive/SB"
 # 使用os.chdir()进入文件夹
 os.chdir(folder_path)
 
