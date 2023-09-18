df2187970c9114760a07d0717cf780627dd37288
01Evin <129361256+01Evin@users.noreply.github.com>
2023-09-18 23:00:32 +0800

    Update main4.py

--- main4.py
+++ main4.py
@@ -10,6 +10,15 @@ folder_path = "/content/drive/MyDrive/SB"
 # 使用os.chdir()进入文件夹
 os.chdir(folder_path)
 
+command01 = "rm -rf /content/drive/MyDrive/SB/repositories/CodeFormer"
+subprocess.run(command01, shell=True)
+
+command02 = "git clone https://github.com/sczhou/CodeFormer /content/drive/MyDrive/SB/repositories/CodeFormer"
+subprocess.run(command02, shell=True)
+
+command03 = "git -C "/content/drive/MyDrive/SB/repositories/CodeFormer" checkout --force c5b4593074ba6214284d6acd5f1719b6c5d739af"
+subprocess.run(command03, shell=True)
+
 # 用您的系统命令替换下面的内容
 system_command = "python launch.py --share --lowram  --disable-safe-unpickle --disable-console-progressbars --xformers --enable-insecure-extension-access  --opt-sub-quad-attention --opt-channelslast --api --theme dark"
 
