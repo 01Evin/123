471f49d0b6d21c2121d6890906ffdc9e963d0eb9
01Evin <129361256+01Evin@users.noreply.github.com>
2023-09-18 23:25:37 +0800

    Update main4.py

--- main4.py
+++ main4.py
@@ -10,13 +10,13 @@ folder_path = "/content/drive/MyDrive/SB"
 # 使用os.chdir()进入文件夹
 os.chdir(folder_path)
 
-command01 = "apt rm -rf /content/drive/MyDrive/SB/repositories/CodeFormer"
+command01 = "rm -rf /content/drive/MyDrive/SB/repositories/CodeFormer"
 subprocess.run(command01, shell=True)
 
-command02 = "apt git clone https://github.com/sczhou/CodeFormer /content/drive/MyDrive/SB/repositories/CodeFormer"
+command02 = "git clone https://github.com/sczhou/CodeFormer /content/drive/MyDrive/SB/repositories/CodeFormer"
 subprocess.run(command02, shell=True)
 
-command03 = command03 = "git -C \"/content/drive/MyDrive/SB/repositories/CodeFormer\" checkout --force 8392d0334956108ab53d9439c4b9fc9c4af0d66d"
+command03 = command03 = "git -C \"/content/drive/MyDrive/SB/repositories/CodeFormer\" checkout --force c5b4593074ba6214284d6acd5f1719b6c5d739af"
 subprocess.run(command03, shell=True)
 
 # 用您的系统命令替换下面的内容
