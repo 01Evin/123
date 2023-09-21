5ea973dfbb49fb560ff528e3799044afdd0d6fa6
01Evin <129361256+01Evin@users.noreply.github.com>
2023-09-21 08:19:13 +0800

    Update main4.py

--- main4.py
+++ main4.py
@@ -19,7 +19,7 @@ subprocess.run(command02, shell=True)
 command03 = "git -C \"/content/drive/MyDrive/SB/repositories/CodeFormer\" checkout --force c5b4593074ba6214284d6acd5f1719b6c5d739af"
 subprocess.run(command03, shell=True)
 
-command04 = "pip install gradio_client==0.5.0"
+command04 = "pip install gradio_client==0.2.7"
 subprocess.run(command04, shell=True)
 
 command05 = "pip install pytorch-lightning==1.7.7"
